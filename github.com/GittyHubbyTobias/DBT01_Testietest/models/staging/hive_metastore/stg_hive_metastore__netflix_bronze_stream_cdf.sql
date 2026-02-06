06e656f5e9ee281e3de2d090175b0212199b2802
Tobias Boeije <tboeije@ilionx.com>
2026-02-06 11:04:48 +0000

    h

---
+++ github.com/GittyHubbyTobias/DBT01_Testietest/models/staging/hive_metastore/stg_hive_metastore__netflix_bronze_stream_cdf.sql
@@ -0,0 +1,27 @@
+with 
+
+source as (
+
+    select * from {{ source('hive_metastore', 'netflix_bronze_stream_cdf') }}
+
+),
+
+renamed as (
+
+    select
+        title_id,
+        title_type,
+        primary_title,
+        original_title,
+        is_adult,
+        start_year,
+        end_year,
+        runtime_minutes,
+        genres,
+        _ingest_ts
+
+    from source
+
+)
+
+select * from renamed
